2ddd307d6b25269ab030053306464a84d35b193c
Trang Vo <65786440+Lucy4788@users.noreply.github.com>
2020-08-01 11:43:18 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,5 @@
+function subscribe(){
+    let text = alert("Thank you for signing up! We'll be in touch ❣");
+}
+let signUpButton = document.querySelector("button");
+signUpButton.addEventListener("click", subscribe);
